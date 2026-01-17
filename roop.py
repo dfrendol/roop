d55a5c2683780f5f2860e428d8c744d1e9253177
UBC Student <dfrendol@students.cs.ubc.ca>
2026-01-17 15:06:21 -0800

    add main in roop.py

--- roop.py
+++ roop.py
@@ -1,1 +1,22 @@
 # entry point
+import os, re, sys, difflib
+from datetime import datetime
+
+def main():
+    if len(sys.argv) < 2:
+        usage()
+        sys.exit(1)
+
+    # parse command
+    cmd = sys.argv[1].lower()
+    if cmd == "init":
+        cmd_init()
+    elif cmd == "update":
+        auto_yes = "--yes" in sys.argv[2:]
+        cmd_update(auto_yes = auto_yes)
+    else:
+        usage()
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()        
